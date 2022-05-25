cda82d4671131f38757a2a4382379c39a969a97f
GiladShapira94 <giladsh@iguazio.com>
2022-05-25 13:59:59 +0000

    Update

--- nuclio-function.py
+++ nuclio-function.py
@@ -1,46 +1,19 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 49,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "import pandas as pd\n",
-    "\n",
-    "def handler(context,event):\n",
-    "    df = pd.read_csv('s3://mlrun-v1-warroom/nyc-taxi-dataset-transformed.csv')\n",
-    "    lasr_drive = df[['hour','day','month','year']].sort_values(by=['year', 'month', 'day', 'hour'],ascending=False).head(1).to_dict(orient='list')\n",
-    "    return lasr_drive"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.6"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 4
-}
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[49]:
+
+
+import pandas as pd
+
+def handler(context,event):
+    df = pd.read_csv('s3://mlrun-v1-warroom/nyc-taxi-dataset-transformed.csv')
+    lasr_drive = df[['hour','day','month','year']].sort_values(by=['year', 'month', 'day', 'hour'],ascending=False).head(1).to_dict(orient='list')
+    return lasr_drive
+
+
+# In[ ]:
+
+
+
+
